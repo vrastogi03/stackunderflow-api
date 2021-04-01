21a271611623667f6dfe85d95ff9183af3472272
Your Name <you@example.com>
2021-04-01 17:08:53 +0530

    adding port

--- server.js
+++ server.js
@@ -206,6 +206,6 @@ app.post('/registerteacher',(req,res)=>{
 
 
 
-app.listen(3000,()=>{
-    console.log('app is running on port 3000');
+app.listen(process.env.PORT||3000,()=>{
+    console.log(`app is running on port ${process.env.PORT}`);
 })
